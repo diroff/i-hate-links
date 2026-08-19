b11fd2849f79e9ff9bab3f81703a7298bf8768b3
Porosb <danil.aprelskiy@mail.ru>
2026-08-19 03:09:54 +0300

    Add Level0 design

---
+++ Assets/_Scripts/Installers/BaseLevelInstaller.cs
@@ -0,0 +1,13 @@
+using Reflex.Core;
+using UnityEngine;
+
+namespace Installers
+{
+    public class BaseLevelInstaller : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+
+        }
+    }
+}
